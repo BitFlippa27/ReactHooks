82041ad64ecd6beed8e539df10a669b6fa455d10
delean <dennishockey19@yahoo.de>
2020-09-25 18:11:25 +0200

    complete todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function ToDo({ todo, index, completeToDo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo" style={{textDecoration: todo.isCompleted ?  "line-through" : ""}}>{todo.text}
+      <div>
+        <button onClick={ () => completeToDo(index)}>Complete</button>
+      </div>
+    </div>
+  );
+}
+
+function ToDoForm({ addToDo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addToDo(value);
+    setValue("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text" 
+        className="input" 
+        value={value} 
+        placeholder="Add ToDo" 
+        onChange={e=> setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build rerally cool todo app",
+      isCompleted: false
+    }
+  ]);
+
+  const addToDo = text => {
+    const newToDos = [...todos, { text }];
+    setTodos(newToDos);
+  }
+
+  const completeToDo = index => {
+    const newToDos = [...todos];
+    newToDos[index].isCompleted = true;
+    setTodos(newToDos);
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <ToDo key={index} index={index} todo={todo} completeToDo={completeToDo} />
+        ))}
+        <ToDoForm addToDo={addToDo} />
+      </div>
     </div>
   );
 }
